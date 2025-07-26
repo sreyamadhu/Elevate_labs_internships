1072e724f01577d2b499817838dd2d530cdcace4
SREYA P <153887888+sreyamadhu@users.noreply.github.com>
2025-07-26 06:30:41 -0400

    Delete campaign directory

--- campaign/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('track/<int:user_id>/', views.track),  # ✅ use 'track' not 'track_click'
-    path('fake-login/', views.fake_login),
-    path('capture/', views.capture_input),
-    path('analytics/', views.plot_events, name='analytics'),
-]
